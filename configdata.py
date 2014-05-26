fb693306f6b82cf12a6d8359594d8c33e2ed7177
hmacread <hugh.macready@gmail.com>
2014-05-26 17:09:20 +0800

    Re-added fixed conflict between origin and master configdata.py

--- configdata.py
+++ configdata.py
@@ -2,9 +2,4 @@ __author__ = 'hmacread'
 
 # Note all these keys are and need to be git cleaned / smudged locally for use in your own environment
 # Instructions for getting an Embed api key here: https://developers.google.com/maps/documentation/embed/guide#api_key
-<<<<<<< HEAD
 GMAPS_EMBED_API_KEY = "AIzaSyBMhILdBdcbYKlKzYg3WeiMfO_Y0tFd-XM"
-
-=======
-GMAPS_EMBED_API_KEY = "AIzaSyBMhILdBdcbYKlKzYg3WeiMfO_Y0tFd-XM"
->>>>>>> 1a84604dc3112087663558bda6c6243b2962e1f5
