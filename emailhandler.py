b20f0433941ff1791b0b27d92255d1d2f675ecca
hmacread <hugh.macready@gmail.com>
2014-05-27 09:03:10 +0800

    TODO added in emailhandler.

--- emailhandler.py
+++ emailhandler.py
@@ -11,16 +11,17 @@ from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 class PositionReportMailHandler(InboundMailHandler):
     
     def receive(self, message):
-        logging.info("Received a message from: " + message.sender)
         plaintext_bodies = message.bodies('text/plain')
         body_count = 0
         for content_type, body in plaintext_bodies:        
             body_count += 1
-            #return an iterator to an upper case only enumeration of email lines    
+            # TODO split out all forwarded emails surrounded by ------ dividers Airmail style here
+            #return an iterator to an upper case only enumeration of email lines
             body_iterator = enumerate(body.decode().splitlines())
             report = positionreport.PositionReport(body_iterator)
             report.put_contents()
 
+        logging.info("Processed a message from: " + message.sender + " with " + str(body_count) + " body(ies).")
         if not body_count > 0:
             raise positionreport.PositionReportError(0, "<No plain text body found in message>")
         
