a0b8304bc70520331fc4232da5f3e8e09e8391d8
hmacread <hugh@macready.id.au>
2014-06-28 01:10:56 +0800

    Updated back to sight public key.

--- configdata.py
+++ configdata.py
@@ -2,5 +2,4 @@ __author__ = 'hmacread'
 
 # Note all these keys are and need to be git cleaned / smudged locally for use in your own environment
 # Instructions for getting an Embed api key here: https://developers.google.com/maps/documentation/embed/guide#api_key
-#GOOGLE_API_KEY = "AIzaSyBMhILdBdcbYKlKzYg3WeiMfO_Y0tFd-XM"
-GOOGLE_API_KEY = "AIzaSyA68r4vc_rhbcClpStxN-PTaiCub3rt83g"
+GOOGLE_API_KEY = "AIzaSyBMhILdBdcbYKlKzYg3WeiMfO_Y0tFd-XM"
